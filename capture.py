4d7a785ba68e73fb43a111d3e21f48ee377fa725
pinae <jonny@pinae.net>
2016-01-17 15:00:05 +0100

    Object oriented

--- capture.py
+++ capture.py
@@ -5,38 +5,36 @@ import pygame
 import sys, os
 
 
-def initialize():
-    pygame.init()
-    display_info = pygame.display.Info()
-    screen_size = (display_info.current_w, display_info.current_h)
-    display_surface = pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
-    return display_surface
+class Capturer(object):
+    def __init__(self):
+        pygame.init()
+        display_info = pygame.display.Info()
+        screen_size = (display_info.current_w, display_info.current_h)
+        self.display_surface = pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
 
+    def display_photo(self):
+        image = pygame.image.load("test.jpg")
+        image_width = min(round(self.display_surface.get_height()/image.get_height()*image.get_width()),
+                          self.display_surface.get_width())
+        image_height = min(round(self.display_surface.get_width()/image.get_width()*image.get_height()),
+                           self.display_surface.get_height())
+        resized_image = pygame.transform.smoothscale(image, (image_width, image_height))
+        self.display_surface.blit(resized_image, (0, 0))
+        pygame.display.update()
 
-def display_photo(surface):
-    image = pygame.image.load("test.jpg")
-    image_width = min(round(surface.get_height()/image.get_height()*image.get_width()),
-                      surface.get_width())
-    image_height = min(round(surface.get_width()/image.get_width()*image.get_height()),
-                       surface.get_height())
-    resized_image = pygame.transform.smoothscale(image, (image_width, image_height))
-    surface.blit(resized_image, (0, 0))
-    pygame.display.update()
-
-
-def handle_inputs():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit(0)
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+    def handle_inputs(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 sys.exit(0)
-            else:
-                print(event.key)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    sys.exit(0)
+                else:
+                    print(event.key)
 
 
 if __name__ == "__main__":
-    display_surface = initialize()
+    capturer = Capturer()
     while True:
-        display_photo(display_surface)
-        handle_inputs()
+        capturer.display_photo()
+        capturer.handle_inputs()
